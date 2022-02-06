e9606e9f5d67e847664f857d7f84c7273dc7f4bc
wlsgp012 <wlsgp012@naver.com>
2022-02-06 10:09:09 +0900

    Update my.el
    Customize rainbow delimiters color

--- personal/my.el
+++ personal/my.el
@@ -11,8 +11,21 @@
 ;; install some packages
 (prelude-require-packages '(rainbow-delimiters color-theme-sanityinc-tomorrow))
 
-;; clojure
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
-
 ;; projectile
 (setq projectile-indexing-method 'hybrid)
+
+;; rainbow-delimiters
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "sienna1"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "pink"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "red")))))
